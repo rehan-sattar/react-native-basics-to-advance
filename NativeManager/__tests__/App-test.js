114cdf6b041761ee2c7d296a1efc08ae8e7cb7f4
rehan-sattar <rehansattar117@gmail.com>
2019-08-08 11:12:19 +0500

    initial commit to manager app with eslint enable

---
+++ NativeManager/__tests__/App-test.js
@@ -0,0 +1,14 @@
+/**
+ * @format
+ */
+
+import 'react-native';
+import renderer from 'react-test-renderer';
+import React from 'react';
+import App from '../App';
+
+// Note: test renderer must be required after react-native.
+
+it('renders correctly', () => {
+  renderer.create(<App />);
+});
