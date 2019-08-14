208bcdd4723ec4afa8cce49426f3f2514dd8916a
rehan-sattar <rehansattar117@gmail.com>
2019-08-14 22:18:24 +0500

    refactor counter app into reducers

--- basics-boiler-plate/src/screens/CounterScreen.js
+++ basics-boiler-plate/src/screens/CounterScreen.js
@@ -1,8 +1,20 @@
-import React, { useState } from 'react';
+import React, { useReducer } from 'react';
 import { View, Text, Button, StyleSheet } from 'react-native';
 
+const counterReducer = (count, action) => {
+  switch (action.type) {
+    case 'increment_counter':
+      return count + 1;
+    case 'decrement_counter':
+      return count > 0 ? count - 1 : count;
+    default:
+      return count;
+  }
+};
+
 const CounterScreen = () => {
-  const [counter, setCounter] = useState(0);
+  const [counter, dispatch] = useReducer(counterReducer, 0);
+
   const {
     containerStyles,
     buttonStyles,
@@ -13,14 +25,14 @@ const CounterScreen = () => {
     <View style={containerStyles}>
       <View style={buttonStyles}>
         <Button
-          onPress={() => setCounter(counter + 1)}
+          onPress={() => dispatch({ type: 'increment_counter' })}
           title="Increment"
           color="#3608DD"
         />
       </View>
       <View style={buttonStyles}>
         <Button
-          onPress={() => setCounter(counter - 1)}
+          onPress={() => dispatch({ type: 'decrement_counter' })}
           title="Decrement"
           color="#3608DD"
         />
