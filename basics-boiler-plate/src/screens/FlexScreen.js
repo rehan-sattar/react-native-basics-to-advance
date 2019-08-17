fecff0581b00c7665fc142dd083cd68ff04f906f
rehan-sattar <rehansattar117@gmail.com>
2019-08-17 23:31:17 +0500

    add directions to flex

--- basics-boiler-plate/src/screens/FlexScreen.js
+++ basics-boiler-plate/src/screens/FlexScreen.js
@@ -1,14 +1,14 @@
-import React from "react";
-import { View, Text, StyleSheet } from "react-native";
+import React from 'react';
+import { View, Text, StyleSheet } from 'react-native';
 
 const FlexScreen = () => {
   return (
     <View style={styles.container}>
       <Text style={styles.headerStyle}>FLEX BOX 101</Text>
-      <View style={[{ flexDirection: "column" }, styles.elementsContainer]}>
-        <View style={{ width: 50, height: 50, backgroundColor: "#EE2C38" }} />
-        <View style={{ width: 50, height: 50, backgroundColor: "#FAA030" }} />
-        <View style={{ width: 50, height: 50, backgroundColor: "#32B76C" }} />
+      <View style={styles.elementsContainer}>
+        <View style={{ width: 50, height: 50, backgroundColor: '#EE2C38' }} />
+        <View style={{ width: 50, height: 50, backgroundColor: '#FAA030' }} />
+        <View style={{ width: 50, height: 50, backgroundColor: '#32B76C' }} />
       </View>
     </View>
   );
@@ -21,13 +21,15 @@ const styles = StyleSheet.create({
   },
   headerStyle: {
     fontSize: 24,
-    textAlign: "center",
-    fontWeight: "100",
+    textAlign: 'center',
+    fontWeight: '100',
     marginBottom: 24
   },
   elementsContainer: {
     flex: 1,
-    backgroundColor: "#ecf5fd",
+    backgroundColor: '#ecf5fd',
+    justifyContent: 'space-around',
+    alignItems: 'center',
     marginLeft: 24,
     marginRight: 24,
     marginBottom: 24
