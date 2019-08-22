ceb6a819beb2a2913923afae4bd4c7532857320a
rehan-sattar <rehansattar117@gmail.com>
2019-08-23 03:39:52 +0500

    fetch business records for inputted search term

--- food-app/src/screens/SearchScreen.js
+++ food-app/src/screens/SearchScreen.js
@@ -1,17 +1,32 @@
 import React, { useState } from 'react';
 import { View, Text } from 'react-native';
 import SearchBar from '../components/SearchBar';
+import yelp from '../api/yelp';
 
 const SearchScreen = () => {
   const [searchTerm, setSearchTerm] = useState('');
+  const [searchResults, setSearchResults] = useState([]);
+
+  searchApi = async () => {
+    const res = await yelp.get('/search', {
+      params: {
+        limit: 50,
+        term: searchTerm,
+        location: 'New York City'
+      }
+    });
+    setSearchResults(res.data.businesses);
+  };
+
   return (
     <View>
       <SearchBar
         searchTerm={searchTerm}
-        onSearchTermChange={searchTerm => setSearchTerm(searchTerm)}
-        onTermSubmit={() => console.log('Submitted')}
+        onSearchTermChange={setSearchTerm}
+        onTermSubmit={searchApi}
       />
       <Text>{searchTerm}</Text>
+      <Text> Saerch results are {searchResults.length}</Text>
     </View>
   );
 };
